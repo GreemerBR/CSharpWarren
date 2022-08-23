bbb8937a0666896d47dca45aff854fe752eccc6a
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-23 16:55:49 -0300

    Inicio da aula de CSharp

---
+++ WarrenTechAcademy/Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using Console._0___TipoValor;
+using Console._99___Exercicios;
+
+namespace Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BemVindo.Mensagem(); // Chamada de método estático
+
+            //TipoNumerico TipoNumerico = new TipoNumerico();
+            //TipoNumerico.Executa();
+
+            //TipoNumerico2.Executa();
+
+            _000Cadastro.Exercicio01();
+
+            System.Console.ReadLine();
+        }
+    }
+}
